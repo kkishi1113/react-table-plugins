e24cc89bdb47d88ab661d7952bda55e42084c266
kkishi1113 <w.destroy1113@gmail.com>
2026-02-14 13:45:48 +0900

    feat: 密度機能の型定義を拡張しました。

--- src/packages/density/density.types.ts
+++ src/packages/density/density.types.ts
@@ -1,4 +1,23 @@
-import type { OnChangeFn, Updater } from "@tanstack/react-table";
+import type { OnChangeFn, RowData, Updater } from "@tanstack/react-table";
+
+declare module "@tanstack/react-table" {
+  interface TableState extends DensityTableState {}
+  interface TableOptionsResolved<
+    TData extends RowData,
+  > extends DensityOptions {}
+  interface Table<TData extends RowData> extends DensityInstance {}
+  // if you need to add cell instance APIs...
+  // interface Cell<TData extends RowData, TValue> extends DensityCell
+  // if you need to add row instance APIs...
+  // interface Row<TData extends RowData> extends DensityRow
+  // if you need to add column instance APIs...
+  // interface Column<TData extends RowData, TValue> extends DensityColumn
+  // if you need to add header instance APIs...
+  // interface Header<TData extends RowData, TValue> extends DensityHeader
+
+  // ノート：`ColumnDef`は型なのでインターフェースではありません。
+  // しかし、それでも`ColumnDef.meta`で宣言マージングを使用できます。
+}
 
 export type DensityState = "sm" | "md" | "lg";
 
